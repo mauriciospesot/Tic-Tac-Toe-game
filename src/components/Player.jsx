f5a5c3b0b4ed16045c459362b859bc5c09fca264
Mauricio <mauricio.spesot@gmail.com>
2024-04-11 13:43:25 -0300

    Create header, player names, edit and save buttons and game board

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
